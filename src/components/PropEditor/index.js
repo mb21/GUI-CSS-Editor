1bf95022ca7d28fcc6fc8c4e06f1f58bf73f9b64
mb21 <mb21@users.noreply.github.com>
2018-01-04 10:38:23 +0100

    change element names

--- src/components/PropEditor/index.js
+++ src/components/PropEditor/index.js
@@ -6,13 +6,13 @@ import {previewBody} from '../../utils'
 
 const elNames = {
   p:  "Paragraph"
-, ul: "Unordered list"
-, ol: "Ordered list"
+, ul: "Bullet list"
+, ol: "Numbered list"
 , li: "List item"
 , h1: "Heading 1"
 , h2: "Heading 2"
 , h3: "Heading 3"
-, blockquote: "Blockquote"
+, blockquote: "Quote"
 , [previewBody]: "Document"
 }
 
